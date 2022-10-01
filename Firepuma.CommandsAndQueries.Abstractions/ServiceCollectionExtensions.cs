d4c732102763bab966719e9123a8ec52e514804a
Francois Hill wootbook <francoishill11@gmail.com>
2022-10-01 21:07:02 +0200

    feat: add ServiceCollectionExtensions to add command auditing

---
+++ Firepuma.CommandsAndQueries.Abstractions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Firepuma.CommandsAndQueries.Abstractions.PipelineBehaviors;
+using Firepuma.CommandsAndQueries.Abstractions.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Firepuma.CommandsAndQueries.Abstractions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddCommandsAuditPipelineBehavior<TAuditStorageImplementation>(
+        this IServiceCollection services)
+        where TAuditStorageImplementation : class, ICommandAuditingStorage
+    {
+        services.AddTransient<ICommandAuditingStorage, TAuditStorageImplementation>();
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuditCommandsBehaviour<,>));
+    }
+}
---
+++ Firepuma.CommandsAndQueries.CosmosDb/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Firepuma.CommandsAndQueries.Abstractions;
+using Firepuma.CommandsAndQueries.Abstractions.Entities;
+using Firepuma.CommandsAndQueries.CosmosDb.Repositories;
+using Firepuma.CommandsAndQueries.CosmosDb.Services;
+using Firepuma.DatabaseRepositories.CosmosDb;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Firepuma.CommandsAndQueries.CosmosDb;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddCommandsAuditWithCosmosDbPipelineBehavior<TPartitionKeyGenerator>(
+        this IServiceCollection services,
+        string commandExecutionEventContainerName)
+        where TPartitionKeyGenerator : class, ICommandAuditPartitionKeyGenerator
+    {
+        services.AddSingleton<ICommandAuditPartitionKeyGenerator, TPartitionKeyGenerator>();
+
+        services.AddCosmosDbRepository<
+            CommandExecutionEvent,
+            ICommandExecutionRepository,
+            CommandExecutionCosmosDbRepository>(
+            commandExecutionEventContainerName,
+            (
+                logger,
+                container,
+                serviceProvider) => new CommandExecutionCosmosDbRepository(logger, container, serviceProvider));
+
+        services.AddCommandsAuditPipelineBehavior<CommandAuditingCosmosDbStorage>();
+    }
+}
