f38838934643e703bf1cdf7e59ac282b990448f2
Francois Hill wootbook <francoishill11@gmail.com>
2023-03-21 04:52:11 +0200

    fix: swap inheritance of BaseCommand (needed after bumping MediatR 12)

--- Firepuma.CommandsAndQueries.Abstractions/Commands/BaseCommand.cs
+++ Firepuma.CommandsAndQueries.Abstractions/Commands/BaseCommand.cs
@@ -1,7 +1,11 @@
 ﻿namespace Firepuma.CommandsAndQueries.Abstractions.Commands;
 
-public abstract class BaseCommand<TResponse> : ICommandRequest<TResponse>
+public abstract class BaseCommand : ICommandRequest
 {
     public string CommandId { get; } = Guid.NewGuid().ToString();
     public DateTime CreatedOn { get; } = DateTime.UtcNow;
+}
+
+public abstract class BaseCommand<TResponse> : BaseCommand, ICommandRequest<TResponse>
+{
 }
